0362b222ec2ba209b609769b3744e8d5d3210efd
SaiVarun107 <saivarunbitra107@gmail.com>
2026-04-27 15:49:32 +0000

    Fix Terraform EKS module configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,19 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "java-cicd-cluster"
+  source  = "terraform-aws-mod  cluster_name    = "java-cicd-cluster"  source  = "terraform-aws-modules/eks/aws"
   cluster_version = "1.29"
 
-  subnet_ids = []   # module creates its own VPC for simplicity
-  vpc_id     = null
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+      instance_types = ["t3.medium"]
+    }
+  }
 }
+``
+  version = "20.8.3"
+
