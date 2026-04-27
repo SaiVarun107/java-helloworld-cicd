589a7a8380898d2296e9b50036e4010d0b96c25b
SaiVarun107 <saivarunbitra107@gmail.com>
2026-04-27 17:16:33 +0000

    Fix EKS module resolution and force upgrade

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,8 @@
 module "eks" {
-  source  = "terraform-aws-mod  cluster_name    = "java-cicd-cluster"  source  = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.3"
+
+  cluster_name    = "java-cicd-cluster"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
@@ -7,13 +10,10 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
       instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
     }
   }
 }
-``
-  version = "20.8.3"
-
